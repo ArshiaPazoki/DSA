30d17146d7668b659ac0832d99ef84d55c8c25a3
arshiapazoki <arshiapazoki2000@gmail.com>
2025-08-14 18:11:43 +0330

    feat: reverseString

---
+++ array/two_pointers/reverseString/reverseString.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def reverseString(self, str: List[str]) -> None:
+        left = 0
+        right = len(str) - 1
+        while left < right:
+            if str[left] != str[right]:
+                str[left],str[right] = str[right],str[left]
+            left += 1
+            right -= 1
+        
+
+if __name__ == "__main__":
+    str = ["h","e","l","l","o"]
+    solution = Solution()
+    solution.reverseString(str)
+    print(str)
